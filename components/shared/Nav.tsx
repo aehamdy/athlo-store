82f2251e324e63072c454babf9dc56292a488e43
AhmedEssam <ae.devverse@gmail.com>
2026-01-28 20:50:55 +0200

    Chore: Import and implement navLinks

--- components/shared/Nav.tsx
+++ components/shared/Nav.tsx
@@ -1,31 +1,11 @@
-import endpoints from "@/lib/endpoints";
-import Link from "next/link";
+import navLinks from "@/lib/navLinks";
 import NavLink from "../NavLink";
-
-const navItems = [
-  {
-    name: "Home",
-    href: endpoints.home,
-  },
-  {
-    name: "Products",
-    href: endpoints.product,
-  },
-  {
-    name: "About",
-    href: endpoints.about,
-  },
-  {
-    name: "Contact",
-    href: endpoints.contact,
-  },
-];
 
 function Nav() {
   return (
     <nav className="">
       <ul className="flex flex-col lg:flex-row items-center gap-md">
-        {navItems.map((item) => (
+        {navLinks.map((item) => (
           <li key={item.name} className="w-full lg:w-fit">
             <NavLink href={item.href} name={item.name} />
           </li>
