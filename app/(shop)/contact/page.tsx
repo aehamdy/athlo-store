4e6709c4c7bb506601a9844c431f2db7a3474706
AhmedEssam <ae.devverse@gmail.com>
2026-02-09 01:53:42 +0200

    Feat: Create 'contact' page

--- app/(shop)/contact/page.tsx
+++ app/(shop)/contact/page.tsx
@@ -1,5 +1,21 @@
+import PageHero from "@/components/layout/PageHero";
+import FAQSection from "@/components/contact/FAQSection";
+import ContactSection from "@/components/contact/ContactSection";
+
 function ContactPage() {
-  return <main>ContactPage</main>;
+  return (
+    <main>
+      <PageHero
+        title="Contact Us"
+        description="Get in touch with us"
+        image="url(https://images.unsplash.com/photo-1534438327276-14e5300c3a48?w=1920)"
+      />
+
+      <ContactSection />
+
+      <FAQSection />
+    </main>
+  );
 }
 
 export default ContactPage;
