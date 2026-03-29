068961a4a1f796cd75e83d963a46b501cd67af97
AhmedEssam <ae.devverse@gmail.com>
2026-03-29 19:44:49 +0200

    Style: Redesign the header to be floating on the screen, and add 'Home' link in nav only on small and medium screens

--- components/HeaderActions.tsx
+++ components/HeaderActions.tsx
@@ -1,12 +1,25 @@
+import Link from "next/link";
 import CartButton from "./CartButton";
 import MobileMenu from "./MobileMenu";
 import ProfileButton from "./ProfileButton";
 import SearchModal from "./SearchModal";
 import PreferencesActions from "./shared/preferences/PreferencesActions";
+import Icon from "./shared/Icon";
+import endpoints from "@/lib/endpoints";
 
 function HeaderActions() {
   return (
     <div className="flex items-center gap-xs">
+      <Link
+        href={endpoints.home}
+        className="group lg:hidden p-xs action-button"
+      >
+        <Icon
+          name="Home"
+          className="group-hover:text-primary-dark group-focus-visible:text-primary-dark"
+        />
+      </Link>
+
       <SearchModal />
 
       <div className="hidden lg:flex lg:items-center lg:gap-xs">
--- components/shared/Nav.tsx
+++ components/shared/Nav.tsx
@@ -6,7 +6,10 @@ function Nav() {
     <nav className="">
       <ul className="flex flex-col lg:flex-row items-center gap-md">
         {navLinks.map((item) => (
-          <li key={item.name} className="w-full lg:w-fit">
+          <li
+            key={item.name}
+            className={`${item.name === "Home" && "hidden lg:block"} w-full lg:w-fit`}
+          >
             <NavLink href={item.href} name={item.name} />
           </li>
         ))}
