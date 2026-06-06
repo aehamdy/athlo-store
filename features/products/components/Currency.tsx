0127788900c69604dbb3e30d07cd944202f0cbb8
AhmedEssam <ae.devverse@gmail.com>
2026-06-06 15:37:51 +0300

    Feat: Add translations for ProductCard components

---
+++ features/products/components/Currency.tsx
@@ -0,0 +1,17 @@
+import { useTranslations } from "next-intl";
+
+type CurrencyProps = {
+  price: number | string;
+};
+
+function Currency({ price }: CurrencyProps) {
+  const t = useTranslations("productCard.currency");
+
+  return (
+    <p className="font-medium text-lg text-primary-light">
+      {t("currency")} {price}
+    </p>
+  );
+}
+
+export default Currency;
