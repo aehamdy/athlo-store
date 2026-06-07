63a2968977a726cc7216b8af20c4fe91603ce853
AhmedEssam <ae.devverse@gmail.com>
2026-06-07 17:54:49 +0300

    Feat: Add privcy policy page with translations

--- app/[locale]/(shop)/privacy-policy/page.tsx
+++ app/[locale]/(shop)/privacy-policy/page.tsx
@@ -1,5 +1,13 @@
+import PolicyContentSection from "@/features/privacy-policy/components/PolicyContentSection";
+import PrivacyPolicyHero from "@/features/privacy-policy/components/PrivacyPolicyHero";
+
 function PrivacyPolicyPage() {
-  return <main>PrivacyPolicyPage</main>;
+  return (
+    <main>
+      <PrivacyPolicyHero />
+      <PolicyContentSection />
+    </main>
+  );
 }
 
 export default PrivacyPolicyPage;
