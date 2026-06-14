e6bbcbe66b01f3f1c73e7883ea3b6c30b921432c
AhmedEssam <reach.ahmedessam@gmail.com>
2026-06-14 20:06:17 +0300

    Refactor: Consolidate language and theme controls into dropdown and remove currency switche

--- components/shared/preferences/PreferencesActions.tsx
+++ components/shared/preferences/PreferencesActions.tsx
@@ -1,13 +1,34 @@
-import CurrencySwitcher from "./CurrencySwitcher";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import LanguageSwitcher from "./LanguageSwitcher";
 import ThemeSwitcher from "./ThemeSwitcher";
+import { Button } from "@/components/ui/button";
+import Icon from "../Icon";
 
 function PreferencesActions() {
   return (
     <div className="flex items-center gap-xs">
-      <LanguageSwitcher />
-      <CurrencySwitcher />
-      <ThemeSwitcher />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="plain" className="action-button">
+            <Icon name="Settings" />
+          </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent>
+          <DropdownMenuItem className="p-0">
+            <LanguageSwitcher />
+          </DropdownMenuItem>
+
+          <DropdownMenuItem className="p-0">
+            <ThemeSwitcher />
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
