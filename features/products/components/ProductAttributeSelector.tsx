f37937dc9a2a53443425d5a059c7b42244ef71c3
AhmedEssam <reach.ahmedessam@gmail.com>
2026-06-28 01:17:31 +0300

    Feat: Support optional attribute selection for product variants

--- features/products/components/ProductAttributeSelector.tsx
+++ features/products/components/ProductAttributeSelector.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@/components/ui/button";
-import { useMemo } from "react";
-import { ProductT } from "../types";
+import { ProductDetails } from "../types";
 
 type ProductAttributeSelectorProps = {
-  attributeKey: string | undefined;
-  product: ProductT;
+  attributeKey?: string;
+  unit?: string;
+  product: ProductDetails;
   selectedAttribute: string;
   setSelectedAttribute: (value: string) => void;
   setSelectedColor: (value: string) => void;
@@ -12,43 +12,51 @@ type ProductAttributeSelectorProps = {
 
 function ProductAttributeSelector({
   attributeKey,
+  unit,
   product,
   selectedAttribute,
   setSelectedAttribute,
   setSelectedColor,
 }: ProductAttributeSelectorProps) {
-  const attributes = useMemo(
-    () =>
-      Array.from(
-        new Set(
-          product.variants?.map((variant) => variant.attributeValueEn) ?? [],
-        ),
-      ),
-    [product.variants],
-  );
+  const attributes = [
+    ...new Set(
+      product.variants
+        .map((variant) => variant.attributeValueEn)
+        .filter(Boolean),
+    ),
+  ];
 
-  const handleSelectSize = (attr: string) => {
-    setSelectedAttribute(attr);
+  const handleSelectAttribute = (value: string) => {
+    setSelectedAttribute(value);
     setSelectedColor("");
   };
 
   return (
-    <div className="flex flex-col gap-sm">
-      {attributeKey && (
-        <p className="font-medium text-sm text-foreground">{attributeKey}:</p>
-      )}
+    <div className="space-y-3">
+      <p className="flex items-center gap-xs font-medium text-sm text-foreground">
+        {attributeKey} :
+        {selectedAttribute && (
+          <span className="text-muted-foreground">
+            {selectedAttribute} {unit && unit}
+          </span>
+        )}
+      </p>
 
-      <div className="flex flex-wrap items-center gap-sm">
-        {attributes.map((attr, idx) => (
-          <Button
-            key={idx}
-            variant={selectedAttribute === attr ? "default" : "outline"}
-            className={`${selectedAttribute === attr ? "text-dark" : "text-foreground"} hover:scale-110`}
-            onClick={() => handleSelectSize(attr)}
-          >
-            {attr}
-          </Button>
-        ))}
+      <div className="flex flex-wrap gap-3">
+        {attributes.map((attribute) => {
+          const selected = attribute === selectedAttribute;
+
+          return (
+            <Button
+              key={attribute}
+              variant={selected ? "default" : "outline"}
+              onClick={() => handleSelectAttribute(attribute)}
+              className={`${selected ? "text-dark" : "text-foreground"} border hover:border-accent-ring`}
+            >
+              {attribute}
+            </Button>
+          );
+        })}
       </div>
     </div>
   );
