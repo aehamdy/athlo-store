de04d3bbbbaffab6f9878bd1e1519bf0f1a60239
AhmedEssam <reach.ahmedessam@gmail.com>
2026-06-30 00:09:03 +0300

    Feat: Implement user logout functionality

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,9 +1,29 @@
+"use client";
+
 import Icon from "./shared/Icon";
 import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+import { useAuthStore } from "@/lib/stores/auth.store";
+import ROUTES from "@/lib/routes";
+import { logout } from "@/lib/auth/auth";
 
 function LogoutButton() {
+  const router = useRouter();
+
+  const handleLogoutClick = () => {
+    logout();
+
+    useAuthStore.getState().logout();
+
+    router.replace(ROUTES.public.home);
+  };
+
   return (
-    <Button variant="destructive" className="w-full text-white">
+    <Button
+      variant="destructive"
+      className="w-full text-white"
+      onClick={handleLogoutClick}
+    >
       <Icon name="LogOut" className="text-current" /> Logout
     </Button>
   );
--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -4,3 +4,8 @@ import { STORAGE_KEYS } from "@/config/constants";
 export function isAuthenticated() {
   return !!Cookies.get(STORAGE_KEYS.accessToken);
 }
+
+export function logout() {
+  Cookies.remove(STORAGE_KEYS.accessToken);
+  Cookies.remove(STORAGE_KEYS.refreshToken);
+}
