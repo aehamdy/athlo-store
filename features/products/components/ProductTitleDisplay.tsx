f3a70a9df714a16fd1d38e972f6781bc467148e4
AhmedEssam <reach.ahmedessam@gmail.com>
2026-07-10 20:11:36 +0300

    Refactor: Change the name of formatName function to formatTitle and use it in related components

--- features/products/components/ProductTitleDisplay.tsx
+++ features/products/components/ProductTitleDisplay.tsx
@@ -1,5 +1,6 @@
 import Heading from "@/components/shared/Heading";
 import { ProductViewVariant } from "../types";
+import formatTitle from "@/lib/utils/formatTitle";
 
 type ProductTitleDisplayProps = {
   title: string;
@@ -7,12 +8,14 @@ type ProductTitleDisplayProps = {
 };
 
 function ProductTitleDisplay({ variant, title }: ProductTitleDisplayProps) {
+  const formattedTitle = formatTitle(title);
+
   return (
     <Heading
-      as="h3"
-      className={`line-clamp-1 ${variant === "details" ? "font-bold text-xl text-foreground" : variant === "quickView" ? "font-medium text-sm text-foreground" : "font-medium text-sm text-muted-foreground"}`}
+      as={variant === "cart-item" ? "h5" : "h3"}
+      className={`line-clamp-1 ${variant === "details" ? "font-bold text-xl text-foreground" : variant === "quickView" ? "font-medium text-sm text-foreground" : variant === "cart-item" ? "font-medium text-sm text-foreground" : "font-medium text-sm text-muted-foreground"}`}
     >
-      {title}
+      {formattedTitle}
     </Heading>
   );
 }
