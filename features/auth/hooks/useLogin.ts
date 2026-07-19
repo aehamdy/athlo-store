cfd0275680191c455461d46df0624d22021e35e8
AhmedEssam <reach.ahmedessam@gmail.com>
2026-07-19 23:04:26 +0300

    Refactor: Add onSuccess and onError actions for login

--- features/auth/hooks/useLogin.ts
+++ features/auth/hooks/useLogin.ts
@@ -1,9 +1,23 @@
+import { AxiosError } from "axios";
 import login from "../api/login";
 import { useMutation } from "@tanstack/react-query";
+import { ApiErrorResponse } from "@/components/shared/types";
+import { toast } from "sonner";
 
 function useLogin() {
   return useMutation({
     mutationFn: login,
+
+    onSuccess: () => {
+      toast.success("You've signed in successfully.");
+    },
+
+    onError: (error: AxiosError<ApiErrorResponse>) => {
+      toast.error(
+        error.response?.data?.message ??
+          "Something went wrong while signing you in.",
+      );
+    },
   });
 }
 
