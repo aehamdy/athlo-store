ace7f6843556cde95604f604d149a414d5b64025
AhmedEssam <reach.ahmedessam@gmail.com>
2026-07-19 23:24:56 +0300

    Refactor: Add onSuccess and onError after user account deletion

--- features/profile/hooks/useDeleteUserAccount.ts
+++ features/profile/hooks/useDeleteUserAccount.ts
@@ -1,9 +1,27 @@
 import { useMutation } from "@tanstack/react-query";
 import deleteUserAccount from "../api/deleteUserAccount";
+import { logout } from "@/lib/auth/auth";
+import { useRouter } from "next/navigation";
+import ROUTES from "@/lib/routes";
+import { toast } from "sonner";
+import { AxiosError } from "axios";
+import { ApiErrorResponse } from "@/components/shared/types";
 
 function useDeleteUserAccount() {
+  const router = useRouter();
+
   return useMutation({
     mutationFn: deleteUserAccount,
+
+    onSuccess: () => {
+      toast.success("Your account has been deleted permenantely");
+      logout();
+      router.push(ROUTES.public.home);
+    },
+
+    onError: (error: AxiosError<ApiErrorResponse>) => {
+      toast.error(error.message ?? "Something went wrong!");
+    },
   });
 }
 
