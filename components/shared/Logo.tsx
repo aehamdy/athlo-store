bd99ce72ef8caec7f9cac934d82bb57f263c035f
AhmedEssam <reach.ahmedessam@gmail.com>
2026-07-29 23:32:06 +0300

    Refactor: Update logo and favicon images

--- components/shared/Logo.tsx
+++ components/shared/Logo.tsx
@@ -1,9 +1,14 @@
+import AppImage from "./AppImage";
+
 function Logo() {
   return (
-    <div className="flex justify-center items-center gap-xs select-none">
-      <span className="py-1 px-2 font-bold uppercase text-lg text-primary-dark bg-accent-base rounded-md">
-        Athlo.
-      </span>
+    <div className="flex justify-center items-center gap-xs select-none bg-accent-base px-xs rounded-md">
+      <AppImage
+        productImage="/images/logo.png"
+        altText="Athlo"
+        width={160}
+        height={40}
+      />
     </div>
   );
 }
