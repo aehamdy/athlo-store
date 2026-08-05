ebd6755ae1a5fc56a63affd9a05a5c437283d7fb
AhmedEssam <reach.ahmedessam@gmail.com>
2026-08-05 23:24:55 +0300

    Feat: Add loading and error states

--- features/home/components/NewArrivalsWrapper.tsx
+++ features/home/components/NewArrivalsWrapper.tsx
@@ -3,12 +3,27 @@
 import ProductCard from "@/features/products/components/ProductCard";
 import useFetchNewArrivals from "../hooks/useFetchNewArrivals";
 import { ProductT } from "@/features/products/types";
+import ProductCardSkeleton from "@/features/products/components/ProductCardSkeleton";
+import ErrorMessage from "@/components/shared/ErrorMessage";
+import { useTranslations } from "next-intl";
 
 function NewArrivalsWrapper() {
-  const { data: newProducts } = useFetchNewArrivals();
+  const t = useTranslations("home.newArrivals");
+  const { data: newProducts, isLoading, isError } = useFetchNewArrivals();
+
+  if (isLoading)
+    return (
+      <div className="grid grid-cols-4 gap-xl">
+        {Array.from({ length: 4 }).map((_, index) => (
+          <ProductCardSkeleton key={index} />
+        ))}
+      </div>
+    );
+
+  if (isError) return <ErrorMessage message={t("failedFetchingNewArrivals")} />;
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-xl">
       {newProducts?.map((product: ProductT) => (
         <ProductCard key={product.id} product={product} />
       ))}
