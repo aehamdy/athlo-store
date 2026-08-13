6da14780cfcc4be42e9d65327f73c4d080f0311d
AhmedEssam <reach.ahmedessam@gmail.com>
2026-08-13 17:31:50 +0300

    Feat: Add language to headers when fetching categories

--- features/categories/hooks/useFetchCategories.ts
+++ features/categories/hooks/useFetchCategories.ts
@@ -1,10 +1,16 @@
+"use client";
+
 import { useQuery } from "@tanstack/react-query";
-import fetchCategories from "../../categories/api/fetchCategories";
+import fetchCategories from "../api/fetchCategories";
+import { CategoriesResponseT } from "../types";
+import { useLocale } from "next-intl";
 
 function useFetchCategories() {
-  return useQuery({
+  const locale = useLocale();
+
+  return useQuery<CategoriesResponseT>({
     queryKey: ["categories"],
-    queryFn: fetchCategories,
+    queryFn: () => fetchCategories(locale),
   });
 }
 
